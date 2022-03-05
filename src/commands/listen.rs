f043f1a6214478abf5faeafe0c4d458c3fd19e13
ltoddy <taoliu0509@gmail.com>
2022-03-05 20:51:49 +0800

    feat: listen command

--- src/commands/listen.rs
+++ src/commands/listen.rs
@@ -1,16 +1,36 @@
-use rdev::{Event, EventType};
+use std::collections::HashMap;
+use std::sync::mpsc::{sync_channel, Receiver, SyncSender};
+use std::thread;
+
+use log::error;
+use rdev::{Event, EventType, Key};
 
 pub fn listen() {
-    if let Err(error) = rdev::listen(callback) {
-        println!("Error: {:?}", error)
+    let (sender, receiver) = sync_channel::<Key>(1024);
+
+    thread::spawn(move || receive_keyboard_press(receiver));
+    // This will block.
+    if let Err(err) = rdev::listen(on_keyboard_press(sender)) {
+        error!("Failed to monitor keyboard: {err:?}");
     }
 }
 
-fn callback(event: Event) {
-    let Event { event_type, .. } = event;
+fn on_keyboard_press(sender: SyncSender<Key>) -> impl Fn(Event) {
+    move |event: Event| {
+        let Event { event_type, .. } = event;
 
-    match event_type {
-        EventType::KeyPress(key) => println!("press key: {key:?}"),
-        _ => (),
+        if let EventType::KeyPress(key) = event_type {
+            let _ = sender.send(key).map_err(|err| error!("failed to send key: {err}"));
+        }
+    }
+}
+
+fn receive_keyboard_press(receiver: Receiver<Key>) {
+    let mut monitor = HashMap::<String, usize>::with_capacity(36);
+
+    for key in receiver {
+        let counter = monitor.entry(format!("{key:?}")).or_insert(0);
+        *counter += 1;
+        println!("monitor is: {monitor:?}");
     }
 }
